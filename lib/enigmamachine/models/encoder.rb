cd4b66f68d16fefe3abaca62850ede983b09205f
dave@netbook <dave@netbook>
2010-03-05 22:55:56 +0000

    Some docs on the Encoder class.

--- lib/enigmamachine/models/encoder.rb
+++ lib/enigmamachine/models/encoder.rb
@@ -3,6 +3,8 @@ require File.dirname(__FILE__) + '/video'
 # An encoding profile which can be applied to a video.  It has a name and is
 # composed of a bunch of EncodingTasks.
 #
+# The Encoder class can shell out to FFMPeg and trigger the encoding of a Video.
+#
 class Encoder
   include DataMapper::Resource
 
@@ -15,8 +17,7 @@ class Encoder
   #
   has n, :encoding_tasks
 
-  # This is a candidate for being moved into the class Encoders::Video (which
-  # could happily be renamed as something like Encoders::Ffmpeg).
+  # Kicks off an FFMpeg encode on a given video.
   #
   def encode(video)
     ffmpeg(encoding_tasks.first, video)
@@ -24,8 +25,9 @@ class Encoder
 
   private
 
-  # If the encode method is pulled into another class, this one should go with
-  # it, obviously.
+  # Shells out to ffmpeg and hits the given video with the parameters in the
+  # given task.  Will call itself recursively until all tasks in this encoder's
+  # encoding_tasks are completed.
   #
   def ffmpeg(task, video)
     current_task_index = encoding_tasks.index(task)
