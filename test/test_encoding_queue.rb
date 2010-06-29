061e979254884a86258df3981777097c7b0e8006
dave <dave@netbook>
2010-06-29 21:02:40 +0100

    Commenting out the encoding queue tests.
    These can go back in once em/spec testing gets ramped up.

--- test/test_encoding_queue.rb
+++ test/test_encoding_queue.rb
@@ -1,34 +1,34 @@
-require File.dirname(__FILE__) + '/helper'
+#require File.dirname(__FILE__) + '/helper'
 
-class TestEncoderQueue < Test::Unit::TestCase
+#class TestEncoderQueue < Test::Unit::TestCase
 
-  context "the encode_next_video method" do
-    setup do
-      destroy_all_videos
-      @video = Video.make
-      @task = EncodingTask.make(:with_encoder)
-      @video.encoder = @task.encoder
-      @video.save
-      sleep 2
-      @queue = EncodingQueue.new
-      @queue.encode_next_video
-    end
+##  context "the encode_next_video method" do
+##    setup do
+##      destroy_all_videos
+##      @video = Video.make
+##      @task = EncodingTask.make(:with_encoder)
+##      @video.encoder = @task.encoder
+##      @video.save
+###      sleep 2
+##      @queue = EncodingQueue.new
+##      @queue.encode_next_video
+##    end
 
-    should "exist" do
-      assert @queue.respond_to? "encode_next_video"
-    end
+##    should_eventually "exist" do
+##      assert @queue.respond_to? "encode_next_video"
+##    end
 
-    should "start encoding the video" do
-      v = Video.get(@video.id)
-      assert_equal "encoding", v.state
-    end
-  end
+##    should_eventually "start encoding the video" do
+##      v = Video.get(@video.id)
+##      assert_equal "encoding", v.state
+##    end
+##  end
 
-  private
+##  private
 
-  def destroy_all_videos
-    Video.all.each { |v| v.destroy! }
-  end
+##  def destroy_all_videos
+##    Video.all.each { |v| v.destroy! }
+##  end
 
-end
+#end
 
