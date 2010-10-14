289166bf1b297ca19d7f67075b3cd059974dd8c6
dave <dave@netbook>
2010-10-14 16:25:58 +0100

    Re-arranged blueprints in alphabetical order.

--- test/support/blueprints.rb
+++ test/support/blueprints.rb
@@ -2,20 +2,9 @@ require 'machinist/data_mapper'
 require 'faker'
 require 'sham'
 
+
 Encoder.blueprint do
   name { Faker::Name.name }
-end
-
-Video.blueprint do
-  encoder
-  file { File.dirname(__FILE__) + "/afile.mpg" }
-  state { "unencoded" }
-  created_at DateTime.now
-  updated_at DateTime.now
-end
-
-Video.blueprint(:with_callback) do
-  callback_url { "http://example.com/call/back/id" }
 end
 
 EncodingTask.blueprint do
@@ -29,5 +18,17 @@ EncodingTask.blueprint(:with_encoder) do
   output_file_suffix { ".foo.flv" }
   command { "-ss 00:00:02 -t 00:00:01 -vcodec mjpeg -vframes 1 -an -f rawvideo -s 180x136"}
   encoder
+end
+
+Video.blueprint do
+  encoder
+  file { File.dirname(__FILE__) + "/afile.mpg" }
+  state { "unencoded" }
+  created_at DateTime.now
+  updated_at DateTime.now
+end
+
+Video.blueprint(:with_callback) do
+  callback_url { "http://example.com/call/back/id" }
 end
 
